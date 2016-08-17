3aa2ba07584dc55be629a6ebd2f16b67cbf98c81
jbrid <jesse.bridge@my.jcu.edu.au>
2016-08-17 20:37:15 +1000

    add name printing program

--- oddName.py
+++ oddName.py
@@ -1,3 +1,13 @@
 """
 Jesse Bridge
 """
+def name_insert():
+    name_error = False
+    while name_error == False:
+        name = input("what is your name")
+        if name == "":
+            print ("error")
+        else:
+            print ("thankyou" + name)
+            name_error = True
+name_insert()
