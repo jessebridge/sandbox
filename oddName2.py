2e13737071affd5d6c5fd35e302085a255cf223f
jbrid <jesse.bridge@my.jcu.edu.au>
2016-08-18 11:24:01 +1000

    checks filse in commit

---
+++ oddName2.py
@@ -0,0 +1,15 @@
+def main():
+    name = get_name()
+    print_name(name)
+
+def print_name(name):
+    for char in range(1, (len(name)), 2):
+        print(name[char])
+
+def get_name():
+    name = input("please enter your name")
+    while name == "":
+        print("error")
+        name = input("please enter your name")
+    return name
+main()
